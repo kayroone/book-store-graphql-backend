0b0a0f0726542620f503d5e90d34d458dc39784e
jan.wiegmann <jan.wiegmann@openknowledge.de>
2020-09-03 14:20:32 +0200

    Initial commit

---
+++ src/test/java/de/jwiegmann/bookstore/GraphQlBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jwiegmann.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
